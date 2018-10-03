ad87d3417e4983600e1c5cc86dc34023042fd9eb
Ashutosh Mestry <amestry@hortonworks.com>
2018-10-03 09:01:07 -0700

    ATLAS-2897: Elegant handling of empty zip files. Part 2.
    Change-Id: Iade992239f720c200b5d2cca274d2ab41d2e93c8
    (cherry picked from commit dfa2c30574fff0290d6a9cb1827de2acf439d909)

--- webapp/src/main/java/org/apache/atlas/web/resources/AdminResource.java
+++ webapp/src/main/java/org/apache/atlas/web/resources/AdminResource.java
@@ -385,7 +385,7 @@ public class AdminResource {
         }
 
         acquireExportImportLock("import");
-        AtlasImportResult result;
+        AtlasImportResult result = null;
 
         try {
             AtlasImportRequest request = AtlasType.fromJson(jsonData, AtlasImportRequest.class);
@@ -394,6 +394,15 @@ public class AdminResource {
             result = importService.run(zipSource, request, Servlets.getUserName(httpServletRequest),
                     Servlets.getHostName(httpServletRequest),
                     AtlasAuthorizationUtils.getRequestIpAddress(httpServletRequest));
+        } catch (AtlasBaseException excp) {
+            if (excp.getAtlasErrorCode().equals(AtlasErrorCode.IMPORT_ATTEMPTING_EMPTY_ZIP)) {
+                LOG.info(excp.getMessage());
+            } else {
+                LOG.error("importData(binary) failed", excp);
+            }
+
+            throw excp;
+
         } catch (Exception excp) {
             LOG.error("importData(binary) failed", excp);
 
@@ -426,6 +435,14 @@ public class AdminResource {
             result = importService.run(request, Servlets.getUserName(httpServletRequest),
                                        Servlets.getHostName(httpServletRequest),
                                        AtlasAuthorizationUtils.getRequestIpAddress(httpServletRequest));
+        } catch (AtlasBaseException excp) {
+            if (excp.getAtlasErrorCode().getErrorCode().equals(AtlasErrorCode.IMPORT_ATTEMPTING_EMPTY_ZIP)) {
+                LOG.info(excp.getMessage());
+            } else {
+                LOG.error("importData(binary) failed", excp);
+            }
+
+            throw excp;
         } catch (Exception excp) {
             LOG.error("importFile() failed", excp);
 
