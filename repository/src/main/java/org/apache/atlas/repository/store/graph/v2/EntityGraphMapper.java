8e19d4499feb95938a2cd7efb4c1697649472d16
Sarath Subramanian <sarath@apache.org>
2019-12-04 20:16:36 -0800

    ATLAS-3545: NullPointerException while trying to delete classification
    (cherry picked from commit c95aba218b9318ce44abf574faff62908f24916d)
    
    Change-Id: If092a031846e5483355846ed6ff6bc0c3b5f7f93

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -1740,6 +1740,10 @@ public class EntityGraphMapper {
         AtlasVertex         classificationVertex = getClassificationVertex(entityVertex, classificationName);
         AtlasClassification classification       = entityRetriever.toAtlasClassification(classificationVertex);
 
+        if (classification == null) {
+            throw new AtlasBaseException(AtlasErrorCode.CLASSIFICATION_NOT_FOUND, classificationName);
+        }
+
         // remove classification from propagated entities if propagation is turned on
         if (isPropagationEnabled(classificationVertex)) {
             List<AtlasVertex> propagatedEntityVertices = deleteDelegate.getHandler().removeTagPropagation(classificationVertex);
@@ -1853,6 +1857,10 @@ public class EntityGraphMapper {
             }
 
             AtlasClassification currentClassification = entityRetriever.toAtlasClassification(classificationVertex);
+
+            if (currentClassification == null) {
+                continue;
+            }
 
             validateAndNormalizeForUpdate(classification);
 
