9d301bee088642e69aea26d38ffa069b85fe04bf
Madhan Neethiraj <madhan@apache.org>
2020-02-21 14:36:45 -0800

    ATLAS-3405: Handling of references to non-existing entities in notifications
    Change-Id: I1b35646ad000bb0bbd6dbf491c1914f9f98e4475
    (cherry picked from commit fcd4123a5519f978a7282fa252fc7998405fcee8)

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v1/EntityGraphRetriever.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v1/EntityGraphRetriever.java
@@ -294,6 +294,7 @@ public final class EntityGraphRetriever {
 
         ret.setTypeName(typeName);
         ret.setGuid(guid);
+        ret.setIsIncomplete(GraphHelper.isEntityIncomplete(entityVertex));
         ret.setStatus(GraphHelper.getStatus(entityVertex));
         ret.setClassificationNames(GraphHelper.getTraitNames(entityVertex));
 
@@ -351,6 +352,7 @@ public final class EntityGraphRetriever {
         }
 
         entity.setGuid(GraphHelper.getGuid(entityVertex));
+        entity.setIsIncomplete(GraphHelper.isEntityIncomplete(entityVertex));
         entity.setTypeName(GraphHelper.getTypeName(entityVertex));
         entity.setStatus(GraphHelper.getStatus(entityVertex));
         entity.setVersion(GraphHelper.getVersion(entityVertex).longValue());
