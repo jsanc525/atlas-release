dd1e8b6b1d7fb9e61b55b52f34be134816a862ee
Madhan Neethiraj <madhan@apache.org>
2020-03-11 09:53:42 -0700

    ATLAS-3614: added configuration to specify default classification-propagation flag
    (cherry picked from commit ba5b2dba53387b11707a073538e7b804be19e0b3)
    
    Change-Id: Iae90fc9b1c062e6ae62a8205952ad47066888e4d

--- repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
+++ repository/src/main/java/org/apache/atlas/repository/store/graph/v2/EntityGraphMapper.java
@@ -115,6 +115,7 @@ public class EntityGraphMapper {
     private static final String CLASSIFICATION_NAME_DELIMITER = "|";
 
     private static final boolean ENTITY_CHANGE_NOTIFY_IGNORE_RELATIONSHIP_ATTRIBUTES = AtlasConfiguration.ENTITY_CHANGE_NOTIFY_IGNORE_RELATIONSHIP_ATTRIBUTES.getBoolean();
+    private static final boolean CLASSIFICATION_PROPAGATION_DEFAULT                  = AtlasConfiguration.CLASSIFICATION_PROPAGATION_DEFAULT.getBoolean();
 
     private final GraphHelper               graphHelper = GraphHelper.getInstance();
     private final AtlasGraph                graph;
@@ -1604,10 +1605,11 @@ public class EntityGraphMapper {
 
                     if(reqContext.isImportInProgress() || reqContext.isInNotificationProcessing()) {
                         propagateTags = false;
-                        classification.setPropagate(propagateTags);
                     } else {
-                        propagateTags = true;
+                        propagateTags = CLASSIFICATION_PROPAGATION_DEFAULT;
                     }
+
+                    classification.setPropagate(propagateTags);
                 }
 
                 if (removePropagations == null) {
