4100779fc9f4b96eba04a5703a73723e42330fbc
gabor.toth <gabor.toth@cloudera.com>
2020-03-19 14:34:05 +0100

    ATLAS-3378: Update JanusGraph to 0.4.0

--- graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasJanusGraph.java
+++ graphdb/janus/src/main/java/org/apache/atlas/repository/graphdb/janus/AtlasJanusGraph.java
@@ -321,7 +321,7 @@ public class AtlasJanusGraph implements AtlasGraph<AtlasJanusVertex, AtlasJanusE
     public void releaseGremlinScriptEngine(ScriptEngine scriptEngine) {
         if (scriptEngine instanceof GremlinGroovyScriptEngine) {
             try {
-                ((GremlinGroovyScriptEngine)scriptEngine).close();
+                ((GremlinGroovyScriptEngine) scriptEngine).reset();
             } catch (Exception e) {
                 // ignore
             }
